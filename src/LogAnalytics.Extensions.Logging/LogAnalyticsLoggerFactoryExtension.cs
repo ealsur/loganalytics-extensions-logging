6f77e10280237a09be1c2e38d18bcc43af55429d
Matias Quaranta <ealsur@users.noreply.github.com>
2018-10-03 14:21:45 -0300

    Adding support for HTTPDataCollectorAPI 1.0.5 (#4)
    * Implementing service endpoint parameter

--- src/LogAnalytics.Extensions.Logging/LogAnalyticsLoggerFactoryExtension.cs
+++ src/LogAnalytics.Extensions.Logging/LogAnalyticsLoggerFactoryExtension.cs
@@ -9,32 +9,34 @@ namespace LogAnalytics.Extensions.Logging
     /// </summary>
     public static class LogAnalyticsLoggerFactoryExtension
     {
-		/// <summary>
-		/// Add Azure LogAnalytics to the logging pipeline.
-		/// </summary>
-		/// <param name="factory">The logger factory to configure.</param>
-		/// <param name="workspaceId">The Azure Log Analytics Workspace Id.</param>
-		/// <param name="key">The API Key of the Azure Log Analytics endpoint</param>
-		/// <returns>The logger factory.</returns>
-		public static ILoggerFactory AddLogAnalytics(this ILoggerFactory factory,string workspaceId, string key, string environmentName,Func<string, LogLevel, bool> filter = null)
+        /// <summary>
+        /// Add Azure LogAnalytics to the logging pipeline.
+        /// </summary>
+        /// <param name="factory">The logger factory to configure.</param>
+        /// <param name="workspaceId">The Azure Log Analytics Workspace Id.</param>
+        /// <param name="key">The API Key of the Azure Log Analytics endpoint</param>
+        /// <param name="serviceNamespace">Optional. Allows to change the service endpoint to use the library in different clouds.</param>
+        /// <returns>The logger factory.</returns>
+        public static ILoggerFactory AddLogAnalytics(this ILoggerFactory factory, string workspaceId, string key, string environmentName, Func<string, LogLevel, bool> filter = null, string serviceNamespace = null)
         {
             if (factory == null) throw new ArgumentNullException(nameof(factory));
 
-            factory.AddProvider(new LogAnalyticsLoggerProvider(filter, workspaceId, key, environmentName));
+            factory.AddProvider(new LogAnalyticsLoggerProvider(filter, workspaceId, key, environmentName, serviceNamespace));
 
             return factory;
         }
 
-		/// <summary>
-		/// Add Azure LogAnalytics to the logging pipeline.
-		/// </summary>
-		/// <param name="factory">The logger factory to configure.</param>
-		/// <param name="workspaceId">The Azure Log Analytics Workspace Id.</param>
-		/// <param name="key">The API Key of the Azure Log Analytics endpoint</param>
-		/// <returns>The logger factory.</returns>
-		public static ILoggerFactory AddLogAnalytics(this ILoggerFactory factory, string workspaceId, string key, string environmentName, LogLevel minLevel)
+        /// <summary>
+        /// Add Azure LogAnalytics to the logging pipeline.
+        /// </summary>
+        /// <param name="factory">The logger factory to configure.</param>
+        /// <param name="workspaceId">The Azure Log Analytics Workspace Id.</param>
+        /// <param name="key">The API Key of the Azure Log Analytics endpoint</param>
+        /// <param name="serviceNamespace">Optional. Allows to change the service endpoint to use the library in different clouds.</param>
+        /// <returns>The logger factory.</returns>
+        public static ILoggerFactory AddLogAnalytics(this ILoggerFactory factory, string workspaceId, string key, string environmentName, LogLevel minLevel, string serviceNamespace = null)
 		{
-			return AddLogAnalytics(factory, workspaceId, key, environmentName,(_, logLevel) => logLevel >= minLevel);
+			return AddLogAnalytics(factory, workspaceId, key, environmentName,(_, logLevel) => logLevel >= minLevel, serviceNamespace);
 		}
 	}
 
